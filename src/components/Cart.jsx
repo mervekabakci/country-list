b907f477383ed0aa3b6f5184238ef998008baf72
TriosMerve <merve@trios.com.tr>
2025-07-01 22:44:33 +0300

    add flag img

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,9 @@
 export default function Cart( {data}){
     return(
         <>
+            <div className="filterColumn">
+                <label htmlFor="filterSelec"></label>
+            </div>
             <h1>Country List</h1>
             <div className="cards">
                 {data.map((item, i) => 
@@ -9,7 +12,7 @@ export default function Cart( {data}){
                         <div><b>Population : </b> {item.population}</div>
                         <div><b>Region : </b> {item.region}</div>
                         <div><b>Capital : </b> {item.capital}</div>
-                        <div><b>Flag : </b> {item.flag}</div>
+                        <div className="flag"><b>Flag : </b> {item.flags.svg ? <img width="40" src={item.flags.svg} alt={item.flags.alt ? item.flags.alt : "" } /> : item.flag}</div>
                     </div>
                 )}
             </div>
