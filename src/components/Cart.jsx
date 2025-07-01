d17aee30cecab6c52d0f7ce78a85ba269501efd4
TriosMerve <merve@trios.com.tr>
2025-07-01 22:57:35 +0300

    add filter region select

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react"
+
 export default function Cart( {data}){
+    const [selectRegion, setSelectRegion] = useState("All");
+
+    const handleChangeRegion = (e) => {
+        setSelectRegion(e.target.value);
+    }
+    const filterRegion = selectRegion === "All" ? data : data.filter(x => x.region === selectRegion);
     return(
         <>
             <div className="filterColumn">
-                <label htmlFor="filterSelec"></label>
+                <label htmlFor="filterSelect">Country Filter</label>
+                <select id="filterSelect" value={selectRegion} onChange={handleChangeRegion}>
+                    <option value="All">All</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Africa">Africa</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Oceania">Oceania</option>
+                    <option value="Americas">Americas</option>
+                </select>
             </div>
             <h1>Country List</h1>
             <div className="cards">
-                {data.map((item, i) => 
+                {filterRegion.map((item, i) => 
                     <div className="card" key={i}>
                         <div className="card-title">{item.name.common}</div>
                         <div><b>Population : </b> {item.population}</div>
